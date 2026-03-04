1f0f690d462cd1f9752e146f5128d3c051a1a2a6
NamanSaxena2029 <namansaxena2029@gmail.com>
2026-03-04 11:47:27 +0530

    Inbox

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : undefined;
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKey: privateKey,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
