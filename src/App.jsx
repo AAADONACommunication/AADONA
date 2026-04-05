aabf6920d3d5e9e5f890bdc6cd6d3e0c9f604109
Naman Saxena <namansaxena2029@gmail.com>
2026-04-06 01:23:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,115 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Blog from "./pages/Blog";
-import About from "./pages/About/About";
-import Csr from "./pages/About/Csr";
-import ContactUs from "./pages/ContactUs";
-import ProjectLocking from "./pages/Partners/ProjectLocking";
-import RequestDemo from "./pages/Partners/RequestDemo";
-import BecomePartner from "./pages/Partners/BecomePartner";
-import RequestTraining from "./pages/Partners/RequestTraining";
-import Careers from "./pages/About/Careers";
-import LeadershipTeam from "./pages/About/LeadershipTeam";
-import MediaCenter from "./pages/About/MediaCenter";
-import MissionVission from "./pages/About/MissionVission";
-import OurCustomer from "./pages/About/OurCustomer";
-import WhistleBlower from "./pages/About/WhistleBlower";
-import WarrantyRegistration from "./pages/support/Warranty-registration";
-import ProductSupport from "./pages/support/Product-support";
-import SupportTools from "./pages/support/Support-tools";
-import CustomersPage from "./Components/CustomersPage";
-import Chatbot from "./Components/Chatbot";
-import TechSquad from "./pages/support/Tech_Squad";
-import Warranty from "./pages/support/Warranty";
-import RequestDoa from "./pages/support/Request_DOA";
-import WarrantyCheck from "./pages/support/WarrantyCheckButton";
-import WhistleBlowerButton from "./pages/About/WhistleBlowerButton";
-import ApplyNow from "./pages/About/ApplyNow";
-import PrivacyPolicy from "./Components/PrivacyPolicy";
-import Breadcrumbs from "./BreadCrumbs";
+// LAZY IMPORTS
+const Home = lazy(() => import("./pages/Home"));
+const Blog = lazy(() => import("./pages/Blog"));
+const About = lazy(() => import("./pages/About/About"));
+const Csr = lazy(() => import("./pages/About/Csr"));
+const ContactUs = lazy(() => import("./pages/ContactUs"));
+const ProjectLocking = lazy(() => import("./pages/Partners/ProjectLocking"));
+const RequestDemo = lazy(() => import("./pages/Partners/RequestDemo"));
+const BecomePartner = lazy(() => import("./pages/Partners/BecomePartner"));
+const RequestTraining = lazy(() => import("./pages/Partners/RequestTraining"));
+const Careers = lazy(() => import("./pages/About/Careers"));
+const LeadershipTeam = lazy(() => import("./pages/About/LeadershipTeam"));
+const MediaCenter = lazy(() => import("./pages/About/MediaCenter"));
+const MissionVission = lazy(() => import("./pages/About/MissionVission"));
+const OurCustomer = lazy(() => import("./pages/About/OurCustomer"));
+const WhistleBlower = lazy(() => import("./pages/About/WhistleBlower"));
+const WarrantyRegistration = lazy(() => import("./pages/support/Warranty-registration"));
+const ProductSupport = lazy(() => import("./pages/support/Product-support"));
+const SupportTools = lazy(() => import("./pages/support/Support-tools"));
+const CustomersPage = lazy(() => import("./Components/CustomersPage"));
+const TechSquad = lazy(() => import("./pages/support/Tech_Squad"));
+const Warranty = lazy(() => import("./pages/support/Warranty"));
+const RequestDoa = lazy(() => import("./pages/support/Request_DOA"));
+const WarrantyCheck = lazy(() => import("./pages/support/WarrantyCheckButton"));
+const WhistleBlowerButton = lazy(() => import("./pages/About/WhistleBlowerButton"));
+const ApplyNow = lazy(() => import("./pages/About/ApplyNow"));
+const PrivacyPolicy = lazy(() => import("./Components/PrivacyPolicy"));
+const CategoryProductsPage = lazy(() => import("./pages/CategoryProductsPage"));
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const AdminPanel = lazy(() => import("./pages/admin/AdminPanel"));
+const ProtectedRoute = lazy(() => import("./pages/admin/ProtectedRoute"));
+const ProductDetailPage = lazy(() => import("./Components/ProductDetailPage"));
+const BlogDetail = lazy(() => import("./pages/Blogdetail"));
 
-import CategoryProductsPage from "./pages/CategoryProductsPage";
-import AdminLogin from "./pages/admin/AdminLogin";
-import AdminPanel from "./pages/admin/AdminPanel";
-import ProtectedRoute from "./pages/admin/ProtectedRoute";
-import ProductDetailPage from "./Components/ProductDetailPage";
-import BlogDetail from "./pages/Blogdetail";
+// Global components (optional lazy bhi kar sakte ho)
+import Breadcrumbs from "./BreadCrumbs";
+import Chatbot from "./Components/Chatbot";
 
 const App = () => {
   return (
     <Router>
 
-      {/* ✅ Global Components */}
+      {/* Global */}
       <Breadcrumbs />
       <Chatbot />
 
-      <Routes>
+      {/* Suspense wrapper */}
+      <Suspense fallback={<div>Loading...</div>}>
 
-        {/* HOME */}
-        <Route path="/" element={<Home />} />
+        <Routes>
 
-        {/* ADMIN */}
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminPanel />
-            </ProtectedRoute>
-          }
-        />
+          <Route path="/" element={<Home />} />
 
-        {/* BLOG */}
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogDetail />} />
+          {/* ADMIN */}
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminPanel />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* ABOUT */}
-        <Route path="/about" element={<About />} />
-        <Route path="/csr" element={<Csr />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/leadershipTeam" element={<LeadershipTeam />} />
-        <Route path="/mediaCenter" element={<MediaCenter />} />
-        <Route path="/missionVision" element={<MissionVission />} />
-        <Route path="/ourCustomers" element={<OurCustomer />} />
-        <Route path="/whistleBlower" element={<WhistleBlower />} />
-        <Route path="/whistleButton" element={<WhistleBlowerButton />} />
-        <Route path="/careers/applyNow" element={<ApplyNow />} />
+          {/* BLOG */}
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogDetail />} />
 
-        {/* CONTACT */}
-        <Route path="/contactus" element={<ContactUs />} />
+          {/* ABOUT */}
+          <Route path="/about" element={<About />} />
+          <Route path="/csr" element={<Csr />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/leadershipTeam" element={<LeadershipTeam />} />
+          <Route path="/mediaCenter" element={<MediaCenter />} />
+          <Route path="/missionVision" element={<MissionVission />} />
+          <Route path="/ourCustomers" element={<OurCustomer />} />
+          <Route path="/whistleBlower" element={<WhistleBlower />} />
+          <Route path="/whistleButton" element={<WhistleBlowerButton />} />
+          <Route path="/careers/applyNow" element={<ApplyNow />} />
 
-        {/* PARTNERS */}
-        <Route path="/projectLocking" element={<ProjectLocking />} />
-        <Route path="/requestDemo" element={<RequestDemo />} />
-        <Route path="/becomePartner" element={<BecomePartner />} />
-        <Route path="/requestTraining" element={<RequestTraining />} />
+          {/* CONTACT */}
+          <Route path="/contactus" element={<ContactUs />} />
 
-        {/* SUPPORT */}
-        <Route path="/productSupport" element={<ProductSupport />} />
-        <Route path="/supportTools" element={<SupportTools />} />
-        <Route path="/warrantyRegistration" element={<WarrantyRegistration />} />
-        <Route path="/requestDoa" element={<RequestDoa />} />
-        <Route path="/techSquad" element={<TechSquad />} />
-        <Route path="/warranty" element={<Warranty />} />
-        <Route path="/warranty/check-Warranty" element={<WarrantyCheck />} />
+          {/* PARTNERS */}
+          <Route path="/projectLocking" element={<ProjectLocking />} />
+          <Route path="/requestDemo" element={<RequestDemo />} />
+          <Route path="/becomePartner" element={<BecomePartner />} />
+          <Route path="/requestTraining" element={<RequestTraining />} />
 
-        {/* OTHERS */}
-        <Route path="/customers" element={<CustomersPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          {/* SUPPORT */}
+          <Route path="/productSupport" element={<ProductSupport />} />
+          <Route path="/supportTools" element={<SupportTools />} />
+          <Route path="/warrantyRegistration" element={<WarrantyRegistration />} />
+          <Route path="/requestDoa" element={<RequestDoa />} />
+          <Route path="/techSquad" element={<TechSquad />} />
+          <Route path="/warranty" element={<Warranty />} />
+          <Route path="/warranty/check-Warranty" element={<WarrantyCheck />} />
 
-        {/* CMS DYNAMIC */}
-        <Route path="/:categoryName" element={<CategoryProductsPage />} />
-        <Route path="/:categoryName/:slug" element={<ProductDetailPage />} />
+          {/* OTHERS */}
+          <Route path="/customers" element={<CustomersPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
 
-      </Routes>
+          {/* CMS */}
+          <Route path="/:categoryName" element={<CategoryProductsPage />} />
+          <Route path="/:categoryName/:slug" element={<ProductDetailPage />} />
+
+        </Routes>
+
+      </Suspense>
     </Router>
   );
 };
