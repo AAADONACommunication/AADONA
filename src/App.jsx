a4ce7c59783621c1865be058a78e56df46742140
NainDembani1 <dembaninain16@gmail.com>
2026-04-09 14:49:24 +0530

    Revert "Merge branch 'main' of https://github.com/NamanSaxena2029/AADONA"
    This reverts commit 11d4e8031c352cbf4836931ff44adfc1e71a077e, reversing
    changes made to 776421df12e874bba38faa3173279677ec5abcb0.

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ const PrivacyPolicy = lazy(() => import("./Components/PrivacyPolicy"));
 const CategoryProductsPage = lazy(() => import("./pages/CategoryProductsPage"));
 const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
 const AdminPanel = lazy(() => import("./pages/admin/AdminPanel"));
-const ProtectedRoute = lazy(() => import("./pages/admin/ProtectedRoute"));
+import ProtectedRoute from "./pages/admin/ProtectedRoute";
 const ProductDetailPage = lazy(() => import("./Components/ProductDetailPage"));
 const BlogDetail = lazy(() => import("./pages/Blogdetail"));
 const NotFound = lazy(() => import("./pages/NotFound"));
