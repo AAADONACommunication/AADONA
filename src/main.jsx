7db15aa9ece93f65d20507074683e5a7de9e2d28
Naman Saxena <namansaxena2029@gmail.com>
2026-04-11 01:58:12 +0530

    p

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,9 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 import '@fontsource/dm-sans/400.css';
-import '@fontsource/dm-sans/500.css';
-import '@fontsource/dm-sans/600.css';
 import '@fontsource/dm-sans/700.css';
 import '@fontsource/outfit/400.css';
 import '@fontsource/outfit/600.css';
-import '@fontsource/outfit/700.css';
-import '@fontsource/outfit/800.css';
-import '@fontsource/poppins/400.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-import '@fontsource/manrope/400.css';
-import '@fontsource/manrope/700.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
