90df37b0841334f2ccf26ff035d76977d07d8654
NainDembani1 <dembaninain16@gmail.com>
2026-04-11 12:22:58 +0530

    images fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.avif";
 import menuIcon from "../assets/menu.svg";
 import closeIcon from "../assets/close.png";
 
